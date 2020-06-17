3d542a5bb5da050bf18ec83e17fed5d296bb1e6f
Haley Maruschak <haley_maruschak@glic.com>
2020-06-17 13:26:45 -0400

    server and folders created

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1> My Makeup Bag </h1>
+
+Product description: 
+The makeup and beauty industry is booming, an industry valued at over $532B and continuing to rise. 
+With the surge of influencers, social media, and the exploding number of brands bring products to market, the supply is outpacing demand with the 
+magnitude of options available to consumers. 
+
+This app wil help you track what you have tried and how you feel about the products. 
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express();//app is an object
+
+require('dotenv').config();
+
+//imported express library
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const app = express();// returns an object
+//const fruits = require('./fruits');
+const routes = require('./routes');
+const jwt = require('jsonwebtoken');
+
+//middleware
+//using body-parser to parse request data
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(3000, ()=>{
+    console.log("I am listening");
+});
