56ca57441bf1e3e14f393691fbd8d52eab7de2fb
Haley Maruschak <haley_maruschak@glic.com>
2020-06-19 14:42:34 -0400

    midday commit 6.19.20

--- seeders/20200619152334-demo-brands.js
+++ seeders/20200619152334-demo-brands.js
@@ -4,8 +4,171 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('brands', [
       {
+        name: 'Anastasia Beverly Hill'
+      },
+      {
+        name: 'Almay'
+      },
+      {
+        name: 'Aveeno'
+      },
+      {
+        name: 'Aquaphor'
+      },
+      {
+        name: 'Bare Republic'
+      },
+      {
+        name: 'bareMinerals'
+      },
+      {
+        name: 'Beauty Bakerie'
+      },
+      {
+        name: 'Benefit Cosmetics'
+      },
+      {
+        name: 'Biore'
+      },
+      {
+        name: 'CeraVe'
+      },
+      {
         name: 'Clinique'
-      }
+      },
+      {
+        name: 'ColourPop'
+      },
+      {
+        name: 'COVER FX'
+      },
+      {
+        name: 'CoverGirl'
+      },
+      {
+        name: 'Dose of Color'
+      },
+      {
+        name: 'e.l.f. Costmetics'
+      },
+      {
+        name: 'Estee Lauder'
+      },
+      {
+        name: 'FLOWER Beauty'
+      },
+      {
+        name: 'House of Lashes'
+      },
+      {
+        name: 'Honest Beauty'
+      },
+      {
+        name: 'IT Cosmetics'
+      },
+      {
+        name: 'J.Cat Beauty'
+      },
+      {
+        name: 'Kate Somerville'
+      },
+      {
+        name: 'LOreal'
+      },
+      {
+        name: 'L.A. Girl'
+      },
+      {
+        name: 'Lancome'
+      },
+      {
+        name: 'Laura Mercier'
+      },
+      {
+        name: 'Lilly Lashes'
+      },
+      {
+        name: 'Lime Crime'
+      },
+      {
+        name: 'LORAC'
+      },
+      {
+        name: 'Lottie London'
+      },
+      {
+        name: 'MAC'
+      },
+      {
+        name: 'Makeup Revolution'
+      },
+      {
+        name: 'Mario Badescu'
+      },
+      {
+        name: 'Milani'
+      },
+      {
+        name: 'Morphe'
+      },
+      {
+        name: 'NABLA'
+      },
+      {
+        name: 'Neutrogena'
+      },
+      {
+        name: 'Nivea'
+      },
+      {
+        name: 'No7'
+      },
+      {
+        name: 'NUDESTIX'
+      },
+      {
+        name: 'NYX Professional Makeup'
+      },
+      {
+        name: 'Ofra Cosmetics'
+      },
+      {
+        name: 'Olay'
+      },
+      {
+        name: 'Pixi'
+      },
+      {
+        name: 'PUR'
+      },
+      {
+        name: 'Revlon'
+      },
+      {
+        name: 'Smashbox'
+      },
+      {
+        name: 'Tarte'
+      },
+      {
+        name: 'Too Faced'
+      },
+      {
+        name: 'UOMA Beauty'
+      },
+      {
+        name: 'Wet n Wild'
+      },
+      {
+        name: 'ZOEVA'
+      },
+      {
+        name: 'Essence'
+      },
+      {
+        name: 'Other'
+      },
+
     ])
   },
 
---
+++ seeders/20200619173405-demo-producttypes.js
@@ -0,0 +1,134 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('ProductTypes', [
+      {
+          name:'Foundation',
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+          name:'Concealer',
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+          name:'Powder',
+          createdAt: new Date(),
+          updatedAt: new Date()
+      },
+      {
+        name:'Primer',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name:'Contour',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+      name:'Bronzer',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name:'Blush',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name:'Highlighter',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name:'Setting spray',
+      createdAt: new Date(),
+      updatedAt: new Date()
+  },
+  {
+    name:'Eyeshadow',
+    createdAt: new Date(),
+    updatedAt: new Date()
+},
+{
+  name:'Eyebrow pomade',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Mascara',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Lashes',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Lip liner',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Lipstick',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Lipgloss',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Eyeliner',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Eye primer',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Makeup remover',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Moisturizer',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Acne Wash',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Body Wash',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+{
+  name:'Other',
+  createdAt: new Date(),
+  updatedAt: new Date()
+},
+
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
