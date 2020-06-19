46994b042098e82d78125a56b3abe9d7fa7cd729
April Murphy <amurphy24@humana.com>
2020-06-19 15:40:37 -0400

    review and user profile linked

--- controllers/review.js
+++ controllers/review.js
@@ -1,5 +1,7 @@
 const Brand = require('../models').brand;
 const Category = require('../models').category;
+const ProductType = require('../models').ProductType;
+const Review = require('../models').Review;
 
 
 
@@ -10,10 +12,17 @@ const review = (req, res) => {
     .then(allBrands => {
         Category.findAll()
         .then(allCategories =>{
-            res.render('review.ejs',{
-                brands:allBrands, //select to put into a dropdown
-                category: allCategories
-            }) 
+            ProductType.findAll()
+            .then(allProductTypes => {
+                res.render('review.ejs',{
+                    brands:allBrands, //select to put into a dropdown
+                    category: allCategories,
+                    productType:allProductTypes,
+                    user:req.params.userId 
+                }) 
+
+            })
+            
         })
        
     
@@ -21,8 +30,17 @@ const review = (req, res) => {
     
 }
 
+const createReview =(req,res)=> {
+    req.body.userId= req.params.userId //this links review to user. this is a variable that can be used anywhere
+    Review.create(req.body)
+    .then(newReview => {
+        res.redirect(`/users/profile/${req.params.userId}`)
+    })
+}
+
 module.exports = {
-    review
+    review,
+    createReview
     // renderReview,
     // reviewSubmission,
     // postReviewRav,
--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
-router.get('/', ctrl.review.review);
+router.get('/:userId', ctrl.review.review);
+router.post('/:userId', ctrl.review.createReview);//this will link the user with the review
 
 
 module.exports = router;
