ae8e4f27431f8b7e34d050098417477a87d6ad3a
April Murphy <amurphy24@humana.com>
2020-06-22 09:53:36 -0400

    dirty coded the hasing stopped on the login section of lesson

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,36 @@
+require('dotenv').config()
+
+const User = require('../models').users;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+
+
 const renderSignup = (req, res) => {
     res.render('signup.ejs')
 }
 
 const signup = (req, res) => {
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return res.status(500).json(err);
+        
+        bcrypt.hash(req.body.password, salt, (err, hashedPwd) => {
+            if (err) return res.status(500).json(err);
+            req.body.password = hashedPwd;
+    
     User.create(req.body)
     .then(newusers => {
         res.redirect(`/users/profile/${newusers.id}`);
     })
-}   
+    .catch(err => {
+        console.log(err);
+        res.send(`err ${err}`);
+         })
+        });
+    });
+}  
+
+
 const renderLogin = (req, res) => {
     res.render('login.ejs') //when rendering a template no forward slash needed .not redirecting to a url
 }
